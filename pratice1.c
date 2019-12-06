21fbefe88488982a470b4b8fedce25d040980cec
Trinadh Venkata Sai Chowadry <36100937+Trinadh1123@users.noreply.github.com>
2019-12-06 10:20:10 +0530

    Update pratice1.c

--- pratice1.c
+++ pratice1.c
@@ -1,10 +1,20 @@
+/** Program to find sum and product of two numbers
+	and sum of individual product of two numbers*/
+
+/* inclusion of header file stdio.h */
+
 #include<stdio.h>
-main()
+
+int main(void)
 {
-	int a,b;
+	
+	int a;
+	int b;
 	printf("Enter any two numbers:");
 	scanf("%d%d",&a,&b);
 	printf("The sum of two numbers is: %d \n",a+b);
 	printf("The product of two numbers is: %d\n",a*b);
 	printf("The sum of the squares of two numbers is: %d",((a*a)+(b*b)));
+	return 0;
+
 }
